88f36a04c6dfded90fce9c1bb754a914ec9011bf
sanliu <sanliu@blizzard.com>
2022-09-27 00:04:06 -0700

    80 --> [add extreme condition]

--- 80. Remove Duplicates from Sorted Array II/Remove Duplicate from Sorted Array II.cs
+++ 80. Remove Duplicates from Sorted Array II/Remove Duplicate from Sorted Array II.cs
@@ -2,10 +2,12 @@ public class Solution
 {
     public int RemoveDuplicates(int[] nums) 
     {
+        if(nums.Length == 0) {return 0;}
+        if(nums.Length == 1) {return 1;}
         int j = 2;
         for(int i = 2; i < nums.Length; i++)
         {
-            if(nums[i] > nums[j-2])
+            if(nums[i] != nums[j-2])
             {
                 nums[j] = nums[i];
                 j++;
